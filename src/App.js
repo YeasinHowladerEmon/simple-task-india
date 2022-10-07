56efc30485ca9fc18801d122f3c2dbaf203289d0
emonibnsalim <emonibnsali@gmail.com>
2022-10-07 10:21:36 +0600

    complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Table } from "reactstrap";
+import "./App.css";
 function App() {
+  const [company, setCompany] = useState([]);
+  useEffect(() => {
+    axios
+      .post("http://demo2211087.mockable.io/mock", {})
+      .then((res) => {
+        setCompany(res.data.companies);
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Company List</h2>
+      <Table hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        {company.map((list, i) => {
+          return (
+            <tbody key={i}>
+              <tr>
+                <th scope="row">{i}</th>
+                <td>{list.name}</td>
+                <td>{list.email}</td>
+                <td>{list.status}</td>
+              </tr>
+            </tbody>
+          );
+        })}
+      </Table>
     </div>
   );
 }
